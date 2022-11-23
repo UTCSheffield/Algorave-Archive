67a1e23945cf83bb44b6b578142a3a09c0692ec8
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-11-23 19:01:59 +0000

    UNDO last commit

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import './webserverModule.js';
 import { schedule } from 'node-cron';
 import downloadModule from './downloadModule.js';
 
+// Set defaults for configuration
+if (typeof process.env.WEBSERVER_ENABLED == 'undefined') {
+    process.env.WEBSERVER_ENABLED = true;
+}
 schedule('0 5 * * TUE', downloadModule);
